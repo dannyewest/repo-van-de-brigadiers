5c62f5307385cf4f7301eb5654e9b38ed53a3462
Jona-A <jonabrands@gmail.com>
2025-10-28 11:21:35 +0100

    + Product-pagina voor review
    de product-pagina is verdeeld in 2 kaarten met rows en kolommen om de mainProduct met details te tonen en de eerst komende geveilde producten.

--- VeilingPlatform/frontend/src/pages/product.jsx
+++ VeilingPlatform/frontend/src/pages/product.jsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import redRosesImage from "../assets/red_roses_bouquet.jpg";
 import whiteRosesImage from "../assets/white_roses_bouquet.jpg";
 import pinkRosesImage from "../assets/pink_roses_bouquet.jpg";
+import Card from "react-bootstrap/Card";
+import { CardText } from "react-bootstrap";
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
 
 const Product = () => {
 
@@ -14,8 +20,8 @@ const Product = () => {
         type: "Flowers",
         potSize: "Medium",
         length: "50 cm",
-        quantity: 100,
-        price: 29.99,
+        quantity: 200,
+        price: 49.99,
         supplier: "Floral Delights",
         auctionDate: "2024-07-15",
         auctionId: 101,
@@ -25,8 +31,8 @@ const Product = () => {
         id: 1,
         name: "White Roses Bouquet",
         type: "Flowers",
-        potSize: "Medium",
-        length: "40 cm",
+        potSize: "Small",
+        length: "38 cm",
         quantity: 80,
         price: 24.99,
         supplier: "Floral Delights",
@@ -36,10 +42,23 @@ const Product = () => {
     },
     {
         id: 3,
-        name: "Pink Roses Bouquet",
+        name: "Pink Roses",
         type: "Flowers",
-        potSize: "Medium",
-        length: "45 cm",
+        potSize: "Large",
+        length: "25 cm",
+        quantity: 72,
+        price: 21.99,
+        supplier: "Floral Delights",
+        auctionDate: "2024-07-15",
+        auctionId: 101,
+        image: pinkRosesImage
+    },
+    {
+        id: 4,
+        name: "Pink Roses",
+        type: "Flowers",
+        potSize: "Large",
+        length: "25 cm",
         quantity: 72,
         price: 21.99,
         supplier: "Floral Delights",
@@ -51,60 +70,64 @@ const Product = () => {
 
     // hoofdproduct en andere producten in aparte staten houden
     const [mainProduct, setMainProduct] = useState(productsData[0]);
-    const otherProducts = productsData.filter((p) => p.id !== mainProduct.id);
+    const otherProducts = productsData.filter((p) => p.id !== mainProduct.id).slice(0, 3);
 
     if (!mainProduct) return <p>Product wordt geladen...</p>;
 
     return (
-        <>
-            <div className="container py-4">
-                {/* Product detail */}
-                <h1 className="mb-3">{mainProduct.name}</h1>
-                <div className="mb-4">
-                    <img
-                    src={mainProduct.image}
-                    alt={mainProduct.name}
-                    className="img-fluid mb-3 rounded float-start me-4"
-                    width="320"
-                    height="400"
-                    />
-                    <div className="mb-3">
-                        <p className="pInfo"><strong>Supplier: </strong>{mainProduct.supplier}</p>
-                        <p className="pInfo"><strong>Veilingdatum: </strong>{mainProduct.auctionDate}</p>
-                        <p className="pInfo"><strong>Type: </strong>{mainProduct.type}</p>
-                        <p className="pInfo"><strong>Length: </strong>{mainProduct.length}</p>
-                        <p className="pInfo"><strong>Quantity: </strong>{mainProduct.quantity}</p>
-                        <p className="pInfo"><strong>Price: </strong>{mainProduct.price}</p>
-                    </div>
-                </div>
-                <button className="btn btn-primary">Plaats bod/Koop</button>
-            </div>
-            <div className="container- mt-5 pt-3">
-                {/* Andere producten - horizontale balk */}
-                <h2 className="mb-3"
-                style={{ fontSize: "18px", fontWeight: "bold" }}>Nog te veilen artikelen:</h2>
-                <div className="overflow-auto white-space-nowrap">
-                    {otherProducts.map((product) => (
-                    <div
-                        key={product.id}
-                        className="d-inline-block m-2 text-center"
-                        onClick={() => setMainProduct(product)}
-                        style={{ cursor: "pointer" }}
-                        
-                    >
-                        <img
-                        src={product.image}
-                        alt={product.name}
-                        className="mb-2 img-thumbnail"
-                        width="120"
-                        height="120"
-                        />
-                        <p className="small">{product.name}</p>
-                    </div>
-                    ))}
-                </div>
-            </div>
-        </>
+        <Container>
+            <Card border="info" style={{ width: '100%' }}>
+                <Card.Header style={{ fontSize: '24px' ,fontWeight: 'bold', textAlign: 'center' }}>{mainProduct.name}</Card.Header>
+                    <Card.Body>
+                        <Row>
+                            <Col xs={12} md={6} className="text-center">
+                                <img
+                                    src={mainProduct.image}
+                                    alt={mainProduct.name}
+                                    className=""
+                                    width="320"
+                                    height="360"
+                                />
+                            </Col>
+                            <Col xs={4} md={3} className="text-align-left mt-3">
+                                <p><strong>Supplier: </strong>{mainProduct.supplier}</p>
+                                <p><strong>AuctionDate: </strong>{mainProduct.auctionDate}</p>
+                                <p><strong>Pot Size: </strong>{mainProduct.potSize}</p>
+                                <p><strong>Type: </strong>{mainProduct.type}</p>
+                                <p><strong>Length: </strong>{mainProduct.length}</p>
+                                <p><strong>Quantity: </strong>{mainProduct.quantity}</p>
+                                <p><strong>Price: </strong>{mainProduct.price}</p>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                <button className="btn btn-success">Place Bid</button>
+            </Card> 
+            <Card border="info" style={{ width: '100%', marginTop: '32px', marginBottom: '20px'}}>
+                <Card.Header style={{ fontSize: '14px'}}>Products to be auctioned</Card.Header>
+                <Card.Body>
+                    <Row>
+                        {otherProducts.map((product) => (
+                        <Col
+                            key={product.id}
+                            className="d-inline-block text-center"
+                            onClick={() => setMainProduct(product)}
+                            style={{ cursor: "pointer" }}
+                            
+                        >
+                            <img
+                            src={product.image}
+                            alt={product.name}
+                            className="img-thumbnail"
+                            width="120"
+                            height="120"
+                            />
+                            <p className="small">{product.name}</p>
+                        </Col>
+                        ))}
+                    </Row>
+                </Card.Body>
+            </Card>           
+        </Container>
     )
 }
 
