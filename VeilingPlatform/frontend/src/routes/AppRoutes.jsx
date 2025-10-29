4c824c81985034f88ce4100dc31fb37c995dfd8e
RobertDevilee1 <24149314@student.hhs.nl>
2025-10-29 10:07:09 +0100

    components aan pagina toegevoegd

--- VeilingPlatform/frontend/src/routes/AppRoutes.jsx
+++ VeilingPlatform/frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "../pages/Login";
+import Login from "../pages/login";
 import AuctionList from "../pages/AuctionList.jsx";
 import NotFound from "../shared/NotFound.jsx";
 import AuctionDetail from "../pages/AuctionDetail.jsx";
@@ -8,7 +8,7 @@ import AuctionDetail from "../pages/AuctionDetail.jsx";
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login />} />
             <Route path="/auction/new" element={<AuctionDetail />} />
             <Route path="/auction/:id" element={<AuctionDetail />} />
             <Route path="/auctions" element={<AuctionList />} />
