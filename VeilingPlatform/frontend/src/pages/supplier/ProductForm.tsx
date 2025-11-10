0d0256e03cc98eac6d62a7c22374ab0c11051605
RobertDevilee1 <24149314@student.hhs.nl>
2025-11-10 11:26:00 +0100

    Update ProductForm.tsx

--- VeilingPlatform/frontend/src/pages/supplier/ProductForm.tsx
+++ VeilingPlatform/frontend/src/pages/supplier/ProductForm.tsx
@@ -126,7 +126,7 @@ function CreateProduct() {
                         <Form.Control
                             type="description"
                             name="description"
-                            placeholder="Image description"
+                            placeholder="Short image description"
                             value={formData.imageDescription}
                         />
                     </Form.Group>
