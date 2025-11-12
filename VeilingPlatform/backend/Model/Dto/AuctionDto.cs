eaa8db75b3c80d587fb0d72bf602c9371175b33a
SvenBrandt <svenfire99@gmail.com>
2025-11-12 11:34:38 +0100

    Read done form DB

---
+++ VeilingPlatform/backend/Model/Dto/AuctionDto.cs
@@ -0,0 +1,22 @@
+namespace VeilingPlatform.Model.Dto
+{
+    public class AuctionDto
+    {
+        public int Id { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+        public List<ProductDto> Products { get; set; } = new();
+    }
+
+    public class CreateAuctionDto
+    {
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+    }
+
+    public class UpdateAuctionDto
+    {
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+    }
+}
