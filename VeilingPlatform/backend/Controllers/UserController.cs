fed0244f91317913a596b8dc00c8a973491cfc82
Haris Mahmood <harisfm2004@gmail.com>
2025-11-13 12:30:36 +0100

    added get endpoints for auctioneer and supplier

--- VeilingPlatform/backend/Controllers/UserController.cs
+++ VeilingPlatform/backend/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using VeilingPlatform.Model;
 namespace VeilingPlatform.Controllers
 {
     [ApiController]
-    [Route("api/register")]
+    [Route("api")]
     public class UserController : ControllerBase
     {
         private readonly DbConnect _context;
@@ -16,7 +16,7 @@ namespace VeilingPlatform.Controllers
             _context = context;
         }
 
-        [HttpPost("user")]
+        [HttpPost("register/user")]
         public async Task<IActionResult> CreateUser(UserDto userDto)
         {
             var existingUser = await _context.Users
@@ -26,7 +26,7 @@ namespace VeilingPlatform.Controllers
             {
                 return Conflict(new { message = "Email already exists. Please use another email." });
             }
-    
+
             var user = new Customer
             {
                 Name = userDto.Name,
@@ -39,5 +39,26 @@ namespace VeilingPlatform.Controllers
 
             return Ok(new { message = "You have successfully signed up! You are being redirected to the login page." });
         }
+
+        [HttpGet("auctioneers")]
+        public async Task<ActionResult<IEnumerable<Auctioneer>>> GetAuctioneers()
+        {
+            var auctioneers = await _context.Users
+                .OfType<Auctioneer>()  
+                .ToListAsync();
+
+            return Ok(auctioneers);
+        }
+
+        [HttpGet("suppliers")]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            var suppliers = await _context.Users
+                .OfType<Supplier>()   
+                .ToListAsync();
+
+            return Ok(suppliers);
+        }
+    
     }
 }
