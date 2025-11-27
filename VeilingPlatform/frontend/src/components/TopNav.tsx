19f0b21abbcf6c03f716bdced4ec0ead0ea870ae
Jona-A <jonabrands@gmail.com>
2025-11-27 10:47:22 +0100

    + ProductSold(POST) & confirmToasts(success/fail)
    routing naar auctionProducts toegevoegd in een button in de cards van auctions op het dashboard van de veilingen.

--- VeilingPlatform/frontend/src/components/TopNav.tsx
+++ VeilingPlatform/frontend/src/components/TopNav.tsx
@@ -37,7 +37,6 @@ export default function TopNav() {
           <Nav className="me-auto">
             <Nav.Link href="/auctions">Auctions</Nav.Link>
             <Nav.Link href="/supplier">Supplier</Nav.Link>
-            <Nav.Link href="/auction/3/products">AuctionProducts</Nav.Link>
           </Nav>
 
           {!user ? (
